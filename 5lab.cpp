392b3f9cd71293fae37d92045aa40af68716804e
Alexandr <166656261+SavkoAlexandr@users.noreply.github.com>
2024-12-15 16:50:17 +0300

    add 5lab

---
+++ 5lab.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cctype>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   setlocale (LC_ALL, "RU");
+    string input;
+    cout << "Введите строку символов: ";
+    getline(cin, input);
+
+    string lowerCase, upperCase, hexNumbers;
+    ostringstream hexStream;
+
+    for (char c : input) {
+        if (isalpha(c)) {
+            lowerCase += tolower(c);
+            upperCase += toupper(c);
+        }
+        else if (isdigit(c)) {
+            int number = c - '0';
+            hexStream << hex << uppercase << number;
+        }
+    }
+
+    string hexResult = hexStream.str();
+    string replacedSpaces = input;
+
+    for (char& c : replacedSpaces) {
+        if (c == ' ') {
+            c = '#';
+        }
+    }
+
+    cout << "Все буквы в нижнем регистре: " << lowerCase << endl;
+    cout << "Все буквы в большом регистре: " << upperCase << endl;
+    cout << "Числа в строке (HEX): " << hexResult << endl;
+    cout << "Строка с заменой пробелов на #: " << replacedSpaces << endl;
+
+    return 0;
+}
